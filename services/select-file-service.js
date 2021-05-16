b93c4d7e94d35b8f3d63ed123ededcc79b3e5973
Daniel Ghazvinie <danielghazvinie@gmail.com>
2021-05-16 08:54:41 +0100

    module 5

--- services/select-file-service.js
+++ services/select-file-service.js
@@ -6,23 +6,23 @@ const dir = process.cwd();
 function getDirectoryContents(files, currentDir, query) {
     const data = [];
     files.forEach((file) => {
-      if (isDirectory(currentDir, file)) {
-        data.push({
-          name : file,
-          isDirectory: true,
-          path : path.join(query, file)
-        });
-      } else {
-        data.push({
-          name : file,
-          isDirectory: false,
-          path : path.join(query, file),
-          currentDir
-        });
-      }
+        if (file.isDirectory(currentDir, file)) {
+            data.push({
+                name: file,
+                isDirectory: true,
+                path: path.join(query, file)
+            });
+        } else {
+            data.push({
+                name: file,
+                isDirectory: false,
+                path: path.join(query, file),
+                currentDir
+            });
+        }
     });
     return data;
-  }
+}
 
 function isDirectory(currentDir, file) {
     const fileInfo = fs.statSync(path.join(currentDir, file));
