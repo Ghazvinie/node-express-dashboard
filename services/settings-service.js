38c5c0740ae5205b237153cea62e86471e4e5f89
Daniel Ghazvinie <danielghazvinie@gmail.com>
2021-05-16 09:14:41 +0100

    module 5

--- services/settings-service.js
+++ services/settings-service.js
@@ -1,14 +1,38 @@
+const fs = require('fs');
+const path = require('path');
+
+const settingsFilePath = path.join(__dirname, '/json/settings.json');
+
 function getSettings() {
-  return {}
+  const settingsData = fs.readFileSync();
+  return JSON.parse(settingsData);
 }
 
 function writeSettings(newSettings) {
+  const settingsJSON = JSON.stringify(newSettings, null, 2);
+  try {
+    fs.writeFileSync(settingsJSON, settingsFilePath);
+    return true;
+  } catch (error) {
+    return false;
+  }
 }
 
 function getDefaultDir() {
+  const defaultDir = getSettings().getDefaultDir;
+  if (!defaultDir) {
+    return process.cwd;
+  }
+  return isValidDir(defaultDir) ? defaultDir : process.cwd;  
 }
 
 function isValidDir(dirPath) {
+  try {
+    fs.readdirSync(dirPath);
+    return true;
+  } catch (error) {
+    return false;    
+  }
 }
 
 module.exports = {
